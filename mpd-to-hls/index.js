990e4aee54835a3224dfc826fdd7c5fe76bc055f
GHIMTUI <103988052+GHIMTUI@users.noreply.github.com>
2025-11-03 12:54:22 +0700

    Delete mpd-to-hls directory

--- mpd-to-hls/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Server Node.js đang chạy trên Render!");
-});
-
-app.listen(10000, () => console.log("Server đang chạy ở cổng 10000"));
