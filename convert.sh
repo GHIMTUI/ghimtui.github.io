c20a1274379776c6fc781c36536479a124bec7ad
GHIMTUI <103988052+GHIMTUI@users.noreply.github.com>
2026-07-06 13:31:56 +0700

    Refactor FFmpeg command for HLS streaming
    Updated FFmpeg command to include additional parameters for video processing and HLS output.

--- convert.sh
+++ convert.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-# Tạo thư mục chứa file xuất ra
 mkdir -p hls
 
-# Chạy FFmpeg để trộn ảnh tĩnh và link radio thành luồng video HLS (.m3u8)
-ffmpeg -loop 1 -i "http://baocamau.vn/live/radio_online.jpg" \
+# Thêm -re để đồng bộ, dùng -preset ultrafast để xử lý siêu nhanh
+# Dùng -t 10 để tạo 1 phân đoạn mồi 10 giây rồi tắt, GitHub sẽ hoàn thành ngay lập tức
+ffmpeg -re -loop 1 -i "http://baocamau.vn/live/radio_online.jpg" \
 -i "http://live-hq.evgcdn.net/live/2851dd3d9016ac74d84b0c4a7a659f76891/chunklist.m3u8" \
--c:v libx264 -tune stillimage -pix_fmt yuv420p -b:v 150k -g 50 \
--c:a copy -shortest \
--f hls -hls_time 6 -hls_playlist_type event hls/tayninh-radio.m3u8
+-c:v libx264 -preset ultrafast -tune stillimage -pix_fmt yuv420p -b:v 50k -g 25 \
+-c:a copy -t 10 \
+-f hls -hls_time 5 -hls_list_size 2 -hls_playlist_type vvod hls/tay-ninh-radio.m3u8
