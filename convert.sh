c3a538bba09844b86e21bb5cb2cacbc46259d168
GHIMTUI <103988052+GHIMTUI@users.noreply.github.com>
2026-07-06 13:17:01 +0700

    Fix FFmpeg command to use correct input URLs

--- convert.sh
+++ convert.sh
@@ -4,8 +4,8 @@
 mkdir -p hls
 
 # Chạy FFmpeg để trộn ảnh tĩnh và link radio thành luồng video HLS (.m3u8)
-ffmpeg -loop 1 -i "http://baocamau.vn/live/radio_online.jpg?id=1" \
--i "http://tv.ctvcamau.vn/live/radio/radio.m3u8" \
+ffmpeg -loop 1 -i "http://baocamau.vn/live/radio_online.jpg" \
+-i "http://live-hq.evgcdn.net/live/2851dd3d9016ac74d84b0c4a7a659f76891/chunklist.m3u8" \
 -c:v libx264 -tune stillimage -pix_fmt yuv420p -b:v 150k -g 50 \
 -c:a copy -shortest \
--f hls -hls_time 6 -hls_playlist_type event hls/camau-radio.m3u8
+-f hls -hls_time 6 -hls_playlist_type event hls/tayninh-radio.m3u8
