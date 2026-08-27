8d7f746cc549972132433547ac6b2830c5d8abe3
GHIMTUI <103988052+GHIMTUI@users.noreply.github.com>
2026-08-27 09:06:16 +0000

    Update channel URLs to use HTTPS

--- .github/workflows/auto_update_link_tivi.py
+++ .github/workflows/auto_update_link_tivi.py
@@ -4,23 +4,23 @@ import requests
 
 # 1. Định nghĩa danh sách các kênh cần lấy và mã tvg-id tương ứng trong file m3u
 CHANNELS = {
-    "sctv2": {"url": "http://vmttv.dpdns.org/VTVGo/?sctv2", "tvg_id": "sctv2hd"},
-    "sctv3": {"url": "http://vmttv.dpdns.org/VTVGo/?sctv3", "tvg_id": "sctv3hd"},
-    "sctv4": {"url": "http://vmttv.dpdns.org/VTVGo/?sctv4", "tvg_id": "sctv4hd"},
-    "sctv7": {"url": "http://vmttv.dpdns.org/VTVGo/?sctv7", "tvg_id": "sctv7hd"},
+    "sctv2": {"url": "https://vmttv.dpdns.org/VTVGo/?sctv2", "tvg_id": "sctv2hd"},
+    "sctv3": {"url": "https://vmttv.dpdns.org/VTVGo/?sctv3", "tvg_id": "sctv3hd"},
+    "sctv4": {"url": "https://vmttv.dpdns.org/VTVGo/?sctv4", "tvg_id": "sctv4hd"},
+    "sctv7": {"url": "https://vmttv.dpdns.org/VTVGo/?sctv7", "tvg_id": "sctv7hd"},
     "sctv8": {"url": "http://vietanhtv.id.vn/sctv/tv.php?id=sctv8-c12d138b", "tvg_id": "sctv8hd"},
-    "sctv9": {"url": "http://vmttv.dpdns.org/VTVGo/?sctv9", "tvg_id": "sctv9hd"},
-    "sctv11": {"url": "http://vmttv.dpdns.org/VTVGo/?sctv11", "tvg_id": "sctv11hd"},
+    "sctv9": {"url": "https://vmttv.dpdns.org/VTVGo/?sctv9", "tvg_id": "sctv9hd"},
+    "sctv11": {"url": "https://vmttv.dpdns.org/VTVGo/?sctv11", "tvg_id": "sctv11hd"},
     "sctv12": {"url": "http://vietanhtv.id.vn/sctv/tv.php?id=sctv12-b0d6c023", "tvg_id": "sctv12hd"},
-    "sctv13": {"url": "http://vmttv.dpdns.org/VTVGo/?sctv13", "tvg_id": "sctv13hd"},
+    "sctv13": {"url": "https://vmttv.dpdns.org/VTVGo/?sctv13", "tvg_id": "sctv13hd"},
     "sctv14": {"url": "http://vietanhtv.id.vn/sctv/tv.php?id=sctv14-789c9f45", "tvg_id": "sctv14hd"},
-    "sctv16": {"url": "http://vmttv.dpdns.org/VTVGo/?sctv16", "tvg_id": "sctv16hd"},
-    "sctv18": {"url": "http://vmttv.dpdns.org/VTVGo/?sctv18", "tvg_id": "sctv18hd"},
+    "sctv16": {"url": "https://vmttv.dpdns.org/VTVGo/?sctv16", "tvg_id": "sctv16hd"},
+    "sctv18": {"url": "https://vmttv.dpdns.org/VTVGo/?sctv18", "tvg_id": "sctv18hd"},
     "sctv19": {"url": "http://vietanhtv.id.vn/sctv/tv.php?id=sctv19-bf00bd02", "tvg_id": "sctv19hd"},
     "sctv21": {"url": "http://vietanhtv.id.vn/sctv/tv.php?id=sctv-21-57de221a", "tvg_id": "sctv21hd"},
-    "sctvphim": {"url": "http://vmttv.dpdns.org/VTVGo/?sctvphim", "tvg_id": "sctvhdpth"},
-   "ondramas": {"url": "http://tv.vietanhtv.top/vieon/vieon.php?id=vie-dramas-hd", "tvg_id": "onviedramas"},
-  "ongiaitri": {"url": "http://tv.vietanhtv.top/vieon/vieon.php?id=vie-giai-tri-hd", "tvg_id": "onviegiaitri"},
+    "sctvphim": {"url": "https://vmttv.dpdns.org/VTVGo/?sctvphim", "tvg_id": "sctvhdpth"},
+   "onviedramas": {"url": "http://tv.vietanhtv.top/vieon/vieon.php?id=vie-dramas-hd", "tvg_id": "onviedramas"},
+  "onviegiaitri": {"url": "http://tv.vietanhtv.top/vieon/vieon.php?id=vie-giai-tri-hd", "tvg_id": "onviegiaitri"},
 
 }
 # Danh sách phân loại kênh theo tần suất
