0bf9288360e84d984383345b1fd3e572140690be
GHIMTUI <103988052+GHIMTUI@users.noreply.github.com>
2026-08-30 05:04:10 +0000

    Add slow_channels.py with streaming channel data

---
+++ .github/workflows/slow_channels.py
@@ -0,0 +1,99 @@
+SLOW_CHANNELS = {
+    "sctv2": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv2",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv2-57c3884d",
+        ],
+        "tvg_id": "sctv2hd",
+    },
+    "sctv3": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv3",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv3-hd-cf3adaca",
+        ],
+        "tvg_id": "sctv3hd",
+    },
+    "sctv4": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv4",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv4-ae779f7e",
+        ],
+        "tvg_id": "sctv4hd",
+    },
+    "sctv7": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv7",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv7-ef7e259a",
+        ],
+        "tvg_id": "sctv7hd",
+    },
+    "sctv9": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv9",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv9-cdd19ab2",
+        ],
+        "tvg_id": "sctv9hd",
+    },
+    "sctv11": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv11",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv11-1d565c47",
+        ],
+        "tvg_id": "sctv11hd",
+    },
+    "sctv12": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv12",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv12-b0d6c023",
+        ],
+        "tvg_id": "sctv12hd",
+    },
+    "sctv13": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv13",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv13-67f88fcb",
+        ],
+        "tvg_id": "sctv13hd",
+    },
+    "sctv16": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv16",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv16-dd1535e8",
+        ],
+        "tvg_id": "sctv16hd",
+    },
+    "sctv18": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctv18",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv18-398b0b4e",
+        ],
+        "tvg_id": "sctv18hd",
+    },
+    "sctv19": {
+        "url": ["http://vietanhtv.id.vn/sctv/tv.php?id=sctv19-bf00bd02"],
+        "tvg_id": "sctv19hd",
+    },
+    "sctv21": {
+        "url": ["http://vietanhtv.id.vn/sctv/tv.php?id=sctv-21-57de221a"],
+        "tvg_id": "sctv21hd",
+    },
+    "sctvphim": {
+        "url": [
+            "https://vmttv.dpdns.org/VTVGo/?sctvphim",
+            "http://tv.vietanhtv.top/vieon/vieon.php?id=sctv-phim-tong-hop-e6d2df88",
+        ],
+        "tvg_id": "sctvhdpth",
+    },
+    "onviedramas": {
+        "url": ["http://tv.vietanhtv.top/vieon/vieon.php?id=vie-dramas-hd"],
+        "tvg_id": "onviedramas",
+    },
+    "onviegiaitri": {
+        "url": ["http://tv.vietanhtv.top/vieon/vieon.php?id=vie-giai-tri-hd"],
+        "tvg_id": "onviegiaitri",
+    },
+    "thanhhoa": {
+        "url": ["http://tv.vietanhtv.top/vieon/vieon.php?id=thanh-hoa-48"],
+        "tvg_id": "thanhhoa",
+    },
+}
